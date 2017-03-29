34c961e302355a52dff736e9b4bb213dfdb267da
Lum1nus <y-U-R-A290@mail.ru>
2017-03-29 22:38:29 +0300

    Add files via upload

--- singlepic_pred.py
+++ singlepic_pred.py
@@ -10,24 +10,24 @@ print 'Model is loaded'
 img = iio.imread('./hdr/anyhere/dani_belgium.hdr', 'HDR-FI')
 rows, cols, depth = img.shape
 
-#l_map = np.zeros((rows,cols))
-#for i in range(rows):
-#    for j in range(cols):
-#        l_map[i,j] = 0.299*img.item(i,j,0) + 0.587*img.item(i,j,1) + 0.114*img.item(i,j,2)
+#~ l_map = np.zeros((rows,cols))
+#~ for i in range(rows):
+    #~ for j in range(cols):
+        #~ l_map[i,j] = 0.299*img.item(i,j,0) + 0.587*img.item(i,j,1) + 0.114*img.item(i,j,2)
         
-#descriptors = []
-#for i in range(rows):
-#    for j in range(cols):
-#        descriptor = np.array(img[i,j,:]) # 3 color channels of the pixel
-#        hist,bins = np.histogram(l_map[max(0,i-14):min(rows, i+14), max(cols//4, j-14):min(j+14, cols)].ravel(), 25, normed = True)
-#        descriptor = np.append(descriptor, hist)
-#        hist,bins = np.histogram(l_map[max(0,i-70):min(rows, i+70), max(cols//4, j-70):min(j+70, cols)].ravel(), 25, normed = True)
-#        descriptor = np.append(descriptor, hist)
-#        descriptors.append(descriptor)
-#descriptors = np.array(descriptors)
+#~ descriptors = []
+#~ for i in range(rows):
+    #~ for j in range(cols):
+        #~ descriptor = np.array(img[i,j,:]) # 3 color channels of the pixel
+        #~ hist,bins = np.histogram(l_map[max(0,i-14):min(rows, i+14), max(0, j-14):min(j+14, cols)].ravel(), 25, normed = True)
+        #~ descriptor = np.append(descriptor, hist)
+        #~ hist,bins = np.histogram(l_map[max(0,i-70):min(rows, i+70), max(0, j-70):min(j+70, cols)].ravel(), 25, normed = True)
+        #~ descriptor = np.append(descriptor, hist)
+        #~ descriptors.append(descriptor)
+#~ descriptors = np.array(descriptors)
 
-#np.save('./img_features/dani_belgium.npy', descriptors)
-#print 'Features are saved'
+#~ np.save('./img_features/dani_belgium.npy', descriptors)
+#~ print 'Features are saved'
 
 descriptors = np.load('./img_features/dani_belgium.npy')
 
@@ -43,7 +43,12 @@ for ii in range(rows):
         img.itemset((ii,jj,1), i)
         img.itemset((ii,jj,2), q)
 
+img *=255
 img[:,:,0] = predictions
+
+#~ print img[100:105, 100:105, 0]
+#~ print img[100:105, 100:105, 1]
+#~ print img[100:105, 100:105, 2]
 
 #convert back to BGR and save img
 for ii in range(rows):
