af2b1183bbf88094a117ff2f4796be8449a9d512
Lum1nus <y-U-R-A290@mail.ru>
2017-03-29 12:51:17 +0300

    Add files via upload

--- singlepic_fit.py
+++ singlepic_fit.py
@@ -7,49 +7,48 @@ np.random.seed(702)# =^_^=
 
 #initial work with images
 input_img = iio.imread('./hdr/anyhere/dani_belgium.hdr', 'HDR-FI')
-true_img = iio.imread('./drago03/anyhere/dani_belgium.png')
+true_img = iio.imread('./reinhard05/anyhere/dani_belgium.png')
 rows, cols, depth = input_img.shape
 
 #build luminance(lightness/brightness/yarkost?) map for hdr one
 l_map = np.zeros((rows,cols))
 for i in range(rows):
-	for j in range(cols):
-		l_map[i,j] = 0.299*input_img.item(i,j,0) + 0.587*input_img.item(i,j,1) + 0.114*input_img.item(i,j,2)
-		#let it be perceptual
+    for j in range(cols):
+        l_map[i,j] = 0.299*input_img.item(i,j,0) + 0.587*input_img.item(i,j,1) + 0.114*input_img.item(i,j,2)
+        #its like conversion to YIQ anyway
 
 #extract descriptors
 descriptors = []
 #whole_hist, bins = np.histogram(l_map.ravel(), 10, normed = True)
 for i in range(rows):
-	for j in range(cols//4, cols):
-		if (i+j)%10==0:
-			descriptor = np.array(input_img[i,j,:]) # 3 color channels of the pixel
-			hist,bins = np.histogram(l_map[max(0,i-14):min(rows, i+14), max(cols//4, j-14):min(j+14, cols)].ravel(), 10, normed = True)
-			descriptor = np.append(descriptor, hist)
-			hist,bins = np.histogram(l_map[max(0,i-70):min(rows, i+70), max(cols//4, j-70):min(j+70, cols)].ravel(), 10, normed = True)
-			descriptor = np.append(descriptor, hist)
-			#descriptor = np.append(descriptor, whole_hist)
-			descriptors.append(descriptor) #add numpy array of features to the array of feature vectors
+    for j in range(cols//4, cols):
+        if (i+j)%10==0:
+            descriptor = np.array(input_img[i,j,:]) # 3 color channels of the pixel
+            hist,bins = np.histogram(l_map[max(0,i-14):min(rows, i+14), max(cols//4, j-14):min(j+14, cols)].ravel(), 25, normed = True)
+            descriptor = np.append(descriptor, hist)
+            hist,bins = np.histogram(l_map[max(0,i-70):min(rows, i+70), max(cols//4, j-70):min(j+70, cols)].ravel(), 25, normed = True)
+            descriptor = np.append(descriptor, hist)
+            descriptors.append(descriptor) #add numpy array of features to the array of feature vectors
 descriptors = np.array(descriptors)
 
 #extract GT
 GT = np.array([])
 for i in range(rows):
-	for j in range(cols//4, cols):
-		if (i+j)%10==0:
-			y, ii, qq = cs.rgb_to_yiq(true_img.item(i,j,0), true_img.item(i,j,1), true_img.item(i,j,2)) #its actually the same with hdr
-			GT = np.append(GT, y)
+    for j in range(cols//4, cols):
+        if (i+j)%10==0:
+            y, ii, qq = cs.rgb_to_yiq(true_img.item(i,j,0), true_img.item(i,j,1), true_img.item(i,j,2)) #its actually the same with hdr
+            GT = np.append(GT, y)
 
 #work with the model
 model = keras.models.Sequential()
-model.add(keras.layers.Dense(200, input_dim=23, activation='relu'))
+model.add(keras.layers.Dense(200, input_dim=53, activation='relu'))
 model.add(keras.layers.Dense(200, activation='relu'))
 model.add(keras.layers.Dense(200, activation='relu'))
 model.add(keras.layers.Dense(1, activation='relu'))
 
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
-model.fit(descriptors, GT, nb_epoch=100, batch_size=256)
+model.fit(descriptors, GT, nb_epoch=100, batch_size=512)
 
 model.save('./models/singlepic.h5')
 
--- singlepic_pred.py
+++ singlepic_pred.py
@@ -1,6 +1,8 @@
 import numpy as np
 import keras
 import imageio as iio
+import colorsys as cs
+import cv2
 
 model = keras.models.load_model('./models/singlepic.h5')
 print 'Model is loaded'
@@ -12,20 +14,21 @@ rows, cols, depth = img.shape
 #for i in range(rows):
 #    for j in range(cols):
 #        l_map[i,j] = 0.299*img.item(i,j,0) + 0.587*img.item(i,j,1) + 0.114*img.item(i,j,2)
-#        
+        
 #descriptors = []
 #for i in range(rows):
 #    for j in range(cols):
 #        descriptor = np.array(img[i,j,:]) # 3 color channels of the pixel
-#        hist,bins = np.histogram(l_map[max(0,i-14):min(rows, i+14), max(cols//4, j-14):min(j+14, cols)].ravel(), 10, normed = True)
+#        hist,bins = np.histogram(l_map[max(0,i-14):min(rows, i+14), max(cols//4, j-14):min(j+14, cols)].ravel(), 25, normed = True)
 #        descriptor = np.append(descriptor, hist)
-#        hist,bins = np.histogram(l_map[max(0,i-70):min(rows, i+70), max(cols//4, j-70):min(j+70, cols)].ravel(), 10, normed = True)
+#        hist,bins = np.histogram(l_map[max(0,i-70):min(rows, i+70), max(cols//4, j-70):min(j+70, cols)].ravel(), 25, normed = True)
 #        descriptor = np.append(descriptor, hist)
 #        descriptors.append(descriptor)
 #descriptors = np.array(descriptors)
 
 #np.save('./img_features/dani_belgium.npy', descriptors)
 #print 'Features are saved'
+
 descriptors = np.load('./img_features/dani_belgium.npy')
 
 predictions = model.predict(descriptors, batch_size=256, verbose=1)
@@ -42,15 +45,17 @@ for ii in range(rows):
 
 img[:,:,0] = predictions
 
-#convert back to RGB and save img
+#convert back to BGR and save img
 for ii in range(rows):
-    for jj in range(cols):
-        r,g,b = cs.yiq_to_rgb(img.item(ii,jj,0), img.item(ii,jj,1), img.item(ii,jj,2))
-        img.itemset((ii,jj,0), r)
-        img.itemset((ii,jj,1), g)
-        img.itemset((ii,jj,2), b)
+	for jj in range(cols):
+		y = img.item(ii,jj,0)
+		i = img.item(ii,jj,1)
+		q = img.item(ii,jj,2)
+		img.itemset((ii,jj,2), y + 0.956*i + 0.623*q)
+		img.itemset((ii,jj,1), y - 0.272*i - 0.648*q)
+		img.itemset((ii,jj,0), y - 1.105*i + 1.705*q)
 
-iio.imwrite('./predicted/dani_belgium.png', img)
+cv2.imwrite('./predicted/dani_belgium.png', img)
 
 
 
